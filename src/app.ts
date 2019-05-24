5420d9bb2565532845104e062b4d9744675a449d
Michael <mihendricks1@gmail.com>
2019-05-24 18:35:24 +0200

    Connected to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
+// Import packages
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 // initialize configuration
 dotenv.config();
 
 // port is now available to the Node.js runtime as if it were and environment variable
 const port = process.env.SERVER_PORT;
+const dbConnectionString = process.env.MONGODB_CONNECTION_STRING as string;
 
 const app = express();
 
@@ -13,7 +17,10 @@ app.get("/", (req, res) => {
   res.send(`Hello world`);
 });
 
-app.listen(port, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`Server started at http://localhost:${port}`);
-});
+mongoose.connect(dbConnectionString)
+  .then((result) => {
+    console.log("Connected to db");
+    app.listen(port, () => {
+      console.log(`Server started at http://localhost:${port}`);
+    });
+  });
